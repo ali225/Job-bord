860f459f6e98cc0a25a1539fc997a459339c03e9
ali225 <aligamal_25@yahoo.com>
2020-11-16 16:08:34 +0200

    add slug and url

--- Job/views.py
+++ Job/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 def job_list(request):
     job_list = Job.objects.all()
 
-    paginator = Paginator(job_list, 3)  # Show 25 contacts per page.
+    paginator = Paginator(job_list, 1)  # Show 25 contacts per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -16,7 +16,7 @@ def job_list(request):
     return render(request, 'job/job_list.html', context)
 
 
-def job_detail(request, id):
-    job_detail = Job.objects.get(id=id)
+def job_detail(request, slug):
+    job_detail = Job.objects.get(slug=slug)
     context = {'job': job_detail}
     return render(request, 'job/job_detail.html', context)
