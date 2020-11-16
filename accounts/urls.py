cd2a79c89347aa4e7a27eedd7ccce18e969c16fa
ali225 <aligamal_25@yahoo.com>
2020-11-17 01:08:31 +0200

    profile and edited login registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,11 @@ from django.urls import include, path
 
 from . import views
 
-app_name='job'
+app_name = 'job'
 
 urlpatterns = [
-    path('signup',views.signup , name='signup'),
-    path('profile',views.profile , name='profile'),
-    path('profile/edit',views.profile_edit , name='profile_edit'),
+    path('signup', views.signup, name='signup'),
+    path('profile', views.profile, name='profile'),
+    path('profile/edit', views.profile_edit, name='profile_edit'),
 
 ]
